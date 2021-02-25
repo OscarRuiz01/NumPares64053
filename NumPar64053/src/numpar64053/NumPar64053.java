0651212999a8fa97f28746e77f77c3f8db5ca494
OscarRuiz <OscarRuiz@OscarRuiz>
2021-02-24 20:51:40 -0600

    Ejercicio#0

---
+++ NumPar64053/src/numpar64053/NumPar64053.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpar64053;
+
+/**
+ *
+ * @author OscarRuiz
+ */
+public class NumPar64053 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int i = 0;
+        while (i < 95){
+            System.out.println("i: " + i++);
+            i++;
+            
+        }
+    }
+    
+}
